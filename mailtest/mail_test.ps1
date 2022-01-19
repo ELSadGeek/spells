9533064c4063c2cfc9d7357b293f42aef8109a60
Jorge <geekmode0n@outlook.com>
2022-01-19 17:48:25 -0600

    master

--- mailtest/mail_test.ps1
+++ mailtest/mail_test.ps1
@@ -9,6 +9,12 @@
 #Leer archivo de configuración JSON
 $SFile = Get-Content -Path "C:\scripts\mail_settings.json" | ConvertFrom-Json
 
+#Preparar datos de envio
+$To = "jorge.fernandez.trueba@outlook.com"
+$Cc = Get-Content -Path "C:\tmp\mail_test\Cc.txt"
+$Att = Get-Content -Path "C:\tmp\mail_test\At.txt"  
+$Cc.ToString()
+$Att.ToString()
 $Subject = "mail_test"
 $Body = "Ahi vamooooo"
 $From = new-object System.Net.Mail.MailAddress($SFile.From, $SFile.Name)
@@ -16,4 +22,4 @@ $Secret = ConvertTo-SecureString $SFile.Secret -AsPlainText -Force
 $Credential = New-Object System.Management.Automation.PSCredential ($SFile.From, $Secret)
 
 #Enviar Correo
-Send-MailMessage -From $From -to $SFile.To -Cc $SFile.Cc1, $SFile.Cc2, $SFile.Cc3 -Subject $Subject -BodyAsHtml $Body -SmtpServer $SFile.SMTP -port $SFile.Port -UseSsl -Credential $Credential -DeliveryNotificationOption OnSuccess
+Send-MailMessage -From $From -to $To -Cc $Cc -Subject $Subject -BodyAsHtml $Body -Attachments $Att -SmtpServer $SFile.SMTP -port $SFile.Port -UseSsl -Credential $Credential -DeliveryNotificationOption OnSuccess
