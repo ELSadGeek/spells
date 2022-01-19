3dc173057bc4e1756fbbec908f87baa33c8bee5d
Jorge <geekmode0n@outlook.com>
2022-01-19 10:05:25 -0600

    master

--- mailtest/mail_test.ps1
+++ mailtest/mail_test.ps1
@@ -9,13 +9,13 @@
 # Variables Mail 
 $smtp_password = ConvertTo-SecureString 'csgcvyvfpqrfwqhn' -AsPlainText -Force
 $smtp_credential = New-Object System.Management.Automation.PSCredential ('facturas@mtinter.com.mx', $smtp_password)
-$From = new-object System.Net.Mail.MailAddress("facturas@mtinter.com.mx", "Facturas MTI")
+$From = new-object System.Net.Mail.MailAddress("facturas@mtinter.com.mx", "facturas@mtinter.com.mx")
 $SMTPServer = "smtp.gmail.com"
 $SMTPPort = "587"
 $To = "jfernandez@mtinter.com.mx"
 $Cc = "nhernandez@mtinter.com.mx"
 $Subject = Get-Date
-$Body = "Prueba, saludos.$From $SMTPServer $SMTPPort $Subject desde $env:computername"
+$Body = "$SMTPServer $SMTPPort $Subject desde $env:computername"
 
 Send-MailMessage -From $From -to $To -Cc $Cc -Subject $Subject -BodyAsHtml $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl -Credential $smtp_credential -DeliveryNotificationOption OnSuccess
 #20:47
