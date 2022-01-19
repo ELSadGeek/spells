6b9ebe4a6ccf50b2a9bb84748020d79e3557b6a1
Jorge <geekmode0n@outlook.com>
2022-01-19 01:28:01 -0600

    master

---
+++ mantos/server.ps1
@@ -0,0 +1,9 @@
+<#
+.Synopsis
+    Mantenimeinto al servidor de los systemas           
+	  
+.Description
+	IT WORKS!
+#>
+
+DISM /Online /Cleanup-Image /RestoreHealth /Source:WIM:D:\install.wim:IndexNumber /LimitAccess
